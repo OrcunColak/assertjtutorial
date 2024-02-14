65a3dcaea598794c0441f9a80e547e00a12b55eb
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-14 11:01:06 +0300

    Initial commit

---
+++ src/test/java/as/AsTest.java
@@ -0,0 +1,22 @@
+package as;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+class AsTest {
+
+    /**
+     * This example shows how to use as() with parameters like String.format()
+     */
+    @Test
+    void testParameterizedMessage(TestInfo testInfo) {
+        String actual = "Hello, World!";
+        String expected = "Hello";
+
+        Assertions.assertThat(actual)
+                .as("%s failed. Expected is different", testInfo.getDisplayName())
+                .isEqualTo(expected);
+    }
+}
